d2ad4f4f3d21bc94235731b9861a1a0bcb9b6994
milanpjevic <milanpjevic@gmail.com>
2020-10-09 15:12:15 +0200

    Reaching out to the Web (http/AJAX)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,25 +7,42 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
-  state = { posts: [] };
+  state = { posts: [], selectedPostId: null };
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      console.log(response);
-      this.setState({
-        posts: response.data.map((post) => {
-          return <Post title={post.title} key={post.id} />;
-        }),
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Milan",
+        };
       });
+      this.setState({ posts: updatedPosts });
     });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
+
     return (
       <div>
-        <section className='Posts'>{this.state.posts}</section>
+        <section className='Posts'>{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
