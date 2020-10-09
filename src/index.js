33f8d7115b14371ffcaa46339df4da9868076676
milanpjevic <milanpjevic@gmail.com>
2020-10-09 19:54:37 +0200

    Adding Interceptors to Execute Code Globaly

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from "axios";
+
+axios.interceptors.request.use(request => {
+    // I can edit requst config before return if I need
+    // console.log(request);
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+});
+
+axios.interceptors.response.use(response => {
+    // I can edit response config before return if I need
+    // console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
