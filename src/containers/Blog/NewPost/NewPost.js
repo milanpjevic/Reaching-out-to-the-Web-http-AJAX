fc7e130cb3ae35e98212fc38cf029cba1d7d34d9
milanpjevic <milanpjevic@gmail.com>
2020-10-10 22:26:02 +0200

    Using Links to Switch Pages

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -32,8 +32,9 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                    <option value="Milan">Milan</option>
                     <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Maximilian">Maximilian</option>
                 </select>
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>
