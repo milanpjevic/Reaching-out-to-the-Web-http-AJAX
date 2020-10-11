5fa15cf6841ed95be6230db22ba3efccbe754de5
milanpjevic <milanpjevic@gmail.com>
2020-10-11 23:11:09 +0200

    Passing Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import { Route, NavLink } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
   render() {
@@ -29,8 +30,9 @@ class Blog extends Component {
               </ul>
             </nav>
           </header>
-          <Route path="/" exact component={Posts}/>
-          <Route path="/new-post" component={NewPost}/>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
         </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,13 +1,12 @@
 import React, {Component} from "react";
 import axios from "axios";
 import Post from "../../../components/Post/Post";
+import { Link } from "react-router-dom";
 import "./Posts.css";
 
 class Posts extends Component {
   state = {
     posts: [],
-    selectedPostId: null,
-    error: false
   };
 
   componentDidMount() {
@@ -38,12 +37,13 @@ class Posts extends Component {
       if(!this.state.error) {
           posts = this.state.posts.map((post) => {
             return (
-              <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-              />
+              <Link to={"/" + post.id} key={post.id}>
+                <Post
+                  title={post.title}
+                  author={post.author}
+                  clicked={() => this.postSelectedHandler(post.id)}
+                />
+              </Link>
             );
         });
       }
