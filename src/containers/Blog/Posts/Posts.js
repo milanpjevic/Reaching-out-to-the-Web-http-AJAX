a19f037302c92db67716b1c8c4de0eb499474d0e
milanpjevic <milanpjevic@gmail.com>
2020-10-13 00:50:51 +0200

    Navigation Programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import axios from "axios";
 import Post from "../../../components/Post/Post";
-import { Link } from "react-router-dom";
+
 import "./Posts.css";
 
 class Posts extends Component {
@@ -28,7 +28,8 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push({pathname: "/" + id})
+    // OR: this.props.history.push("/" + id)
   };
 
   render() {
@@ -37,13 +38,14 @@ class Posts extends Component {
       if(!this.state.error) {
           posts = this.state.posts.map((post) => {
             return (
-              <Link to={"/" + post.id} key={post.id}>
+              // <Link to={"/" + post.id} >
                 <Post
+                  key={post.id}
                   title={post.title}
                   author={post.author}
                   clicked={() => this.postSelectedHandler(post.id)}
                 />
-              </Link>
+              // </Link>
             );
         });
       }
